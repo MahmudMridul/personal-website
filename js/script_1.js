e46e17b6a020be20cdacafc666876e6aa6e4dd1d
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-06-29 22:33:18 +0600

    added smooth scrolling

--- js/script_1.js
+++ js/script_1.js
@@ -1,3 +1,5 @@
+var scroll = new SmoothScroll('.side-menu-text a', { speed: 700 });
+
 window.onscroll = function()
 {
 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -5,3 +7,4 @@ window.onscroll = function()
 	var scroll = (winScroll / height) * 100;
 	document.getElementById("scrollBar").style.width = scroll + "%";
 };
+
